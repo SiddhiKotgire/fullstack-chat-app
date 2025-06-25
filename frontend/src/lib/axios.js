40384f39f6949df1479876cc0002e65b681db97e
Siddhi Kotgire <kotgiresiddhi56@gmail.com>
2025-06-25 08:59:44 +0530

    Update axios baseURL logic

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:import.meta.env.MODE ==="development"? "https://fullstack-chat-app-6-mw51.onrender.com":"/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-6-mw51.onrender.com" : "/api",
   withCredentials: true,
 });
